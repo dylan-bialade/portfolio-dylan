951859900303c627fd185cc464ca66c7c33a810e
dylan-bialade <dylan.bialade@gmail.com>
2025-11-21 09:30:37 +0100

    modif footer pour gganalytics

--- partials/footer.php
+++ partials/footer.php
@@ -15,13 +15,13 @@
       yearSpan.textContent = new Date().getFullYear();
     }
   })();
-  
- async src="https://www.googletagmanager.com/gtag/js?id=G-82KCXG3K0Q">
+</script>
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-82KCXG3K0Q"></script>
+<script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
   gtag('config', 'G-82KCXG3K0Q');
-
 </script>
